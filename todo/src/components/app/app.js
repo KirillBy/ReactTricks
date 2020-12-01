a036fecc76c13e4b980acf52b47ef0593d9056c4
Kirill <balanovichks@gmail.com>
2020-12-02 00:06:37 +0300

    finished todo without filters and search

--- todo/src/components/app/app.js
+++ todo/src/components/app/app.js
@@ -8,13 +8,25 @@ import './app.css';
 import ItemAddForm from '../item-add-form';
 
 export default class App extends Component {
+
+  maxId = 100;
   state = {
     todoData: [
-      { label: 'Drink Coffee', important: false, id: 1 },
-      { label: 'Make Awesome App', important: true, id: 2 },
-      { label: 'Have a lunch', important: false, id: 3 }
+      this.createTodoItem('Drink Beer'),
+      this.createTodoItem('Learn React'),
+      this.createTodoItem('Eat Pizza')
     ]
   };
+
+  createTodoItem(label) {
+    
+    return {
+      label,
+      important: false,
+      done: false,
+      id: this.maxId++
+    }
+  }
 
   deleteItem = (id) => {
     this.setState(({todoData}) => {
@@ -30,23 +42,55 @@ export default class App extends Component {
 
   addItem = (text) => {
     this.setState(({todoData}) => {
-      const idx = todoData.length + 1;
-      const newArray = [...todoData, {label: text, important: false, id: idx}];
+      const newArray = [...todoData, this.createTodoItem(text)];
       return {
         todoData: newArray
       }
     })
   }
 
+  toggleProperty = (arr, id, propName) => {
+    const idx = arr.findIndex((el) => el.id === id);
+    const oldItem = arr[idx];
+    const newItem = {...oldItem, [propName]: !oldItem[propName]};
+    return [...arr.slice(0, idx), newItem, ...arr.slice(idx +1)];
+    
+  }
+
+  onToggleImportant = (id) => {
+    this.setState(({todoData}) => {
+      return{
+        todoData: this.toggleProperty(todoData, id, 'important')
+      };
+    });
+  };
+
+  onToggleDone = (id) => {
+    this.setState(({todoData}) => {
+      return{
+        todoData: this.toggleProperty(todoData, id, 'done')
+      };
+    })
+  };
+
   render() {
+    const { todoData } = this.state;
+    const doneCount = todoData
+                      .filter((el) => el.done).length;
+    const todoCount = todoData.length - doneCount;
     return (
       <div className="todo-app">
-        <AppHeader toDo={1} done={3} />
+        <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
           <SearchPanel />
           <ItemStatusFilter />
         </div>
-        <TodoList todos={this.state.todoData} onDeleted={ this.deleteItem} />
+        <TodoList 
+        todos={todoData} 
+        onDeleted={ this.deleteItem} 
+        onToggleImportant={this.onToggleImportant}
+        onToggleDone = {this.onToggleDone}
+        />
         <ItemAddForm onAdd ={this.addItem}/>
       </div>
     );
--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -1,32 +1,7 @@
 import React, {Component} from 'react';
 import './todo-list-item.css';
 
-export default class TodoListItem extends Component {
-
-  state = {
-    done: false,
-    important: false
-  };
-
-  onLabelClick = () => {
-    this.setState(({done}) => {
-      return {
-        done: !done
-      };
-    });
-  };
-
-  onMarkImportant = () => {
-    this.setState(({important}) => {
-      return {
-        important: !important
-      };
-    });
-  };
-  
-  render() {
-    const {label, onDeleted} = this.props;
-    const {done, important} = this.state;
+const TodoListItem = ({label, onDeleted, onToggleImportant, onToggleDone, done, important}) => {
 
     let classNames = 'todo-list-item';
     if(done) {
@@ -41,13 +16,13 @@ export default class TodoListItem extends Component {
       <span className={classNames}>
         <span
           className="todo-list-item-label"
-          onClick={ this.onLabelClick}>
+          onClick={ onToggleDone}>
           {label}
         </span>
   
         <button type="button"
                 className="btn btn-outline-success btn-sm float-right"
-                onClick = {this.onMarkImportant}>
+                onClick = {onToggleImportant}>
           <i className="fa fa-exclamation" />
         </button>
   
@@ -58,6 +33,6 @@ export default class TodoListItem extends Component {
         </button>
       </span>
     );
-  }
 }
 
+export default TodoListItem;
